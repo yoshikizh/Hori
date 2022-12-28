3e400cc6d1c277fbe320d15be2e7ccb848476c6b
yoshikizh <177365340@qq.com>
2022-12-28 17:43:44 +0800

    add default log configuration.

--- tests/config/env/development.js
+++ tests/config/env/development.js
@@ -5,26 +5,29 @@ module.exports = {
   // For log config
   logConfiguration: {
 
+    // output folder
     output: "./logs",
 
-    // console | file | dateFile
-    type: "dateFile",  
+    // console | file
+    type: "file",  
 
-    // out put level 
+    // output lowest level
     level: "debug",    
 
     // blow if avaliable if type is file or dataFile
     file: {
-      filename: "development",
+      filename: "development.log",
       compress: true,
+      maxLogSize: 10240000,
+      backups: 3,
 
-      // only for dataFile
-      pattern: "yyyy-MM-dd-hh.log", 
-      alwaysIncludePattern: true
+      // if set to true, will enable the pattern suffix
+      alwaysIncludePattern: true,
+      pattern: "yyyy-MM-dd"
     }
   }
 
   // For Application
-
+  // Hori.config can get this file object directly.
 
 }
