4392e7c0eb485d361145f9c81318fe7e7468de01
yoshikizh <177365340@qq.com>
2022-12-29 15:07:11 +0800

    update scaffold.

--- scaffold/new/config/env/env.js
+++ scaffold/new/config/env/env.js
@@ -1,7 +1,37 @@
 module.exports = {
-	// For Hori
-	appName: "Your application name",
-	logDir: `logs`,
+  // For Hori
+  appName: "Your application name",
 
-	// For Application
+  // speed up for development
+  // Files under the app and config folders will be monitor.
+  autoRestartWhenFileChanged: true,
+  autoRestartMonitorInterval: 2000,
+
+  // For log config
+  logConfiguration: {
+
+    // output folder
+    output: "./logs",
+
+    // console | file
+    type: "console",  
+
+    // output lowest level
+    level: "debug",
+
+    // blow if avaliable if type is file or dataFile
+    file: {
+      filename: "development.log",
+      compress: true,
+      maxLogSize: 10240000,
+      backups: 3,
+
+      // if set to true, will enable the pattern suffix
+      alwaysIncludePattern: true,
+      pattern: "yyyy-MM-dd"
+    }
+  }
+
+  // For Application
+  // Hori.config can get this file object directly.
 }
